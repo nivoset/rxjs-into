519ed001250d70790630fc95b6eb62345938610e
Ben Koop <benjamin.koop@cardinalhealth.com>
2021-08-10 08:54:16 -0400

    cleaning up

--- src/into.spec.ts
+++ src/into.spec.ts
@@ -4,18 +4,18 @@ import { into } from './into'
 
 it('should pipe the new item into the existing Subject/Behavior subject', (done) => {
 
-  const main = new Subject<string>();
+  const main = new Subject<string>()
 
   main.asObservable().pipe(take(1)).subscribe((val) => {
     expect(val).toBe('TEST')
   }, undefined, done)
 
-  of<string>('TEST').pipe(into(main));
+  of<string>('TEST').pipe(into(main))
 })
 
 it('should pipe multiple items into the existing Subject/Behavior subject', (done) => {
 
-  const main = new Subject<string>();
+  const main = new Subject<string>()
 
   main.asObservable().pipe(take(2)).subscribe((val) => {
     expect(val).toBe('TEST')
